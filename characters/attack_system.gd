8b647f95369947942be838b14bedd7bb1cd3b282
Julien Letellier <letellier.julien@gmail.com>
2021-11-16 17:39:30 +0100

    Add attack cooldown and pushback

--- characters/attack_system.gd
+++ characters/attack_system.gd
@@ -1,9 +1,31 @@
 extends Node
 
 export var strength = 100
+export var cooldown = 1
+
+var target_health_system = null
+var last_hit_delta = cooldown
+
+onready var character = $"../Character"
 
 
 func _on_HitBox_body_entered(body: Node):
 	var health_system = body.get_node_or_null("../HealthSystem")
 	if health_system:
-		health_system.take_damage(strength)
+		target_health_system = health_system
+
+
+func _on_HitBox_body_exited(body: Node):
+	var health_system = body.get_node_or_null("../HealthSystem")
+	if health_system == target_health_system:
+		target_health_system = null
+
+
+func _physics_process(delta):
+	if target_health_system:
+		if last_hit_delta > cooldown:
+			var origin_position = character.position
+			target_health_system.take_damage(strength, origin_position)
+			last_hit_delta = 0
+
+	last_hit_delta += delta
--- characters/health_system.gd
+++ characters/health_system.gd
@@ -2,8 +2,16 @@ extends Node
 
 export var health = 100
 
-func take_damage(strength: int):
+
+func take_damage(strength: int, origin_position = null):
     health -= strength
+
+    if origin_position:
+        var character = get_node("../Character")
+        var delta_vector = character.position - origin_position
+        delta_vector = delta_vector.normalized()
+        character.position += delta_vector * 8
+        character.snapped_position = Vector2.ZERO
 
     if (health <= 0):
         get_parent().queue_free()
