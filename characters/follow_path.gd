a0b89a0de783e045b560c528c51cf2f4f2b84cc9
Julien Letellier <letellier.julien@gmail.com>
2021-11-19 11:40:48 +0100

    Improve path follow system for enemies

--- characters/follow_path.gd
+++ characters/follow_path.gd
@@ -3,28 +3,28 @@ extends Node
 const Character := preload("res://characters/character.gd")
 
 export var path := NodePath()
+export var loop: bool = false
 
+var current_point_idx: int = 0
 var direction: int = 1
 
-onready var path_follow_2d := get_node(path) as PathFollow2D
+onready var path_2d := get_node(path) as Path2D
+onready var curve := path_2d.curve
 onready var character := get_parent() as Character
 
 
 func _physics_process(delta: float) -> void:
-	if path_follow_2d.unit_offset >= 0.99 && direction == 1:
-		direction = -1
-	elif path_follow_2d.unit_offset <= 0.01 && direction == -1:
-		direction = 1
+	var point_count := curve.get_point_count()
+	var current_point := path_2d.position + curve.get_point_position(current_point_idx)
+	var old_position := character.position
+	var path_direction := (current_point - old_position).normalized()
 
-	var path_direction = Vector2(cos(path_follow_2d.rotation), sin(path_follow_2d.rotation))
-	path_direction = path_direction * direction
-
-	var oldPosition = character.position
 	character.rotation = path_direction.angle()
 	character.move(delta, path_direction)
-	var currentPosition = character.position
-	var distance = currentPosition.distance_to(oldPosition)
-	path_follow_2d.offset += distance * direction
 
-	if character.position.distance_to(path_follow_2d.position) > 2:
-		character.position = path_follow_2d.position
+	if character.position.distance_to(current_point) < 1:
+		current_point_idx += direction
+		if loop:
+			current_point_idx = current_point_idx % point_count
+		elif current_point_idx == 0 || current_point_idx == point_count - 1:
+			direction *= -1
