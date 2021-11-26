ed0c9fb1065e5ba07a524ca8be68791d8dfd31fe
Julien Letellier <letellier.julien@gmail.com>
2021-11-26 12:15:25 +0100

    Finish entire dialogue for the first level

--- characters/follow_character_action.gd
+++ characters/follow_character_action.gd
@@ -3,9 +3,16 @@ extends Node
 const FollowCharacter := preload("res://characters/follow_character.gd")
 const Character := preload("res://characters/character.gd")
 
+export var is_active: bool = true
+
 onready var _player := $"../../Player" as Character
 onready var _follow_character := $"../FollowCharacter" as FollowCharacter
 
 
-func trigger_action():
-	_follow_character.set_target(_player)
+func trigger_action() -> void:
+	if is_active:
+		_follow_character.set_target(_player)
+
+
+func set_is_active(value: bool) -> void:
+	is_active = value
