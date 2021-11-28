94b283c48b0ebb7a08eadfb70a64e4aaf133e2b8
Julien Letellier <letellier.julien@gmail.com>
2021-11-28 16:37:36 +0100

    Add simple particles to poison trap

--- items/poison_trap.gd
+++ items/poison_trap.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+const ParticleRes := preload("res://items/poison_trap_particles.tscn")
 const HealthSystem := preload("res://characters/health_system.gd")
 
 
@@ -7,5 +8,10 @@ func _on_Item_body_entered(body: Node) -> void:
 	var health_system := body.get_node_or_null("HealthSystem") as HealthSystem
 	if health_system:
 		health_system.take_damage(100)
+
+	var particle := ParticleRes.instance() as CPUParticles2D
+	particle.position = position
+	particle.emitting = true
+	get_parent().add_child(particle)
 
 	queue_free()
